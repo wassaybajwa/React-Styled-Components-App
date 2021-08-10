b5050eb4a2bd45a868ef44e8e6f80244b70aadb9
wassaybajwa <wassaybajwa@hotmail.com>
2021-08-10 03:33:31 -0400

    Button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Styled from "styled-components";
 
+const BasicTitle = Styled.h1`
+  text-align: center;
+  text-transform: capitalize;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem' }}>
+      <BasicTitle>Styled Component</BasicTitle>
+      <button className="btn">Click me</button>
+ 
     </div>
   );
 }
